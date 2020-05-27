43acc2f3bc3bc014811e70e01657265053b3b886
Kgg9 <Kartikeyg910@gmail.com>
2020-05-26 21:08:19 -0400

    Updated

--- Genius_Lyrics.py
+++ Genius_Lyrics.py
@@ -1,1 +1,78 @@
-print("Hello World")
+import requests
+import urllib3
+from urllib.parse import quote
+from bs4 import BeautifulSoup
+import json
+import re
+from fake_useragent import UserAgent
+
+ua = UserAgent()
+headers = {'User-Agent':ua.random}
+
+base = "https://api.genius.com/"
+ss = "search?q="
+Song = quote("free porn cheap drugs")
+token = "&access_token=1wsoiHas6wuszSvEKNWofVuD9NKiXnXXWzeuwayw3I56CIoGIakOv1ecbYzO4mfW"
+
+Qstring = base + ss + Song + token
+
+res = requests.get(Qstring)
+src = res.json()
+
+options = {}
+
+for i in src['response']['hits']:
+    name_title  = (i["result"]["primary_artist"]["name"]) + ":" + " " +   (i["result"]["title_with_featured"])
+    api_bruh = (i["result"]["api_path"])
+    options[name_title] = api_bruh
+
+
+
+d = 1
+song_id = list(options.values())
+for i in options:
+    print(f"{d}.{i}")
+    d+=1
+
+
+def picking():
+    while True:
+        cho = int(input("Enter Song Number "))
+        try:
+            song_url = song_id[cho-1]
+            return song_url
+        except:
+            print("Incorrect Number Bruh")
+            return picking()
+
+'''def picking():
+    cho = int(input("Enter Song Number "))
+    try:
+        while cho != song_id.index(song_id[cho-1]): [Althernative way but other way is better??]
+            song_url = song_id[cho-1]
+            return song_url
+    except:
+        print("Incorrect Number Bruh")
+        return picking()'''
+
+SongURL = f"https://genius.com{picking()}"
+
+res_1 = requests.get(SongURL)
+soup = BeautifulSoup(res_1.text, "html5lib")
+
+lyrics = soup.find("div", class_="lyrics").get_text()
+
+print(lyrics)
+
+
+
+
+
+
+
+
+
+
+
+
+
